630245e03c0b1ef13c3aa05aa6de291bd1f580eb
Jason Axelson <jason.axelson@gmail.com>
2024-09-10 05:14:01 -1000

    Initial commit

---
+++ lib/log_viz.ex
@@ -0,0 +1,18 @@
+defmodule LogViz do
+  @moduledoc """
+  Documentation for `LogViz`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LogViz.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/log_viz_test.exs
@@ -0,0 +1,8 @@
+defmodule LogVizTest do
+  use ExUnit.Case
+  doctest LogViz
+
+  test "greets the world" do
+    assert LogViz.hello() == :world
+  end
+end
